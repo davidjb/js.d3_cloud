e5422eeda5ce646d23b83b6a27354f37997e26c9
David\ Beitey <david@davidjb.com>
2013-02-01 12:07:25 +1000

    Conditional rendering of script tag for IE>=9

--- CHANGES.rst
+++ CHANGES.rst
@@ -4,7 +4,9 @@ CHANGES
 1.0.1-1 (unreleased)
 ====================
 
-- Nothing changed yet.
+- Render d3_cloud's script tag with conditional comment for IE >=9 (what
+  ``js.d3`` supports).
+  [davidjb]
 
 
 1.0.1 (2012-12-21)
--- js/d3_cloud/__init__.py
+++ js/d3_cloud/__init__.py
@@ -1,9 +1,10 @@
 from fanstatic import Library, Resource
-from js.d3 import d3
+from js.d3 import d3, render_js_d3_new_ie
 
 library = Library('d3_word_cloud', 'resources')
 
 d3_cloud = Resource(library, 'd3.layout.cloud.js',
                     minified='d3.layout.cloud.min.js',
-                    depends=(d3,)
+                    depends=(d3,),
+                    renderer=render_js_d3_new_ie
                    )
