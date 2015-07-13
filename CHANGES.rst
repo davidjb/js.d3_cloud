a428c2eb677c96d0f3a060c6ac18a1ee4c3907c0
David\ Beitey <david@davidjb.com>
2015-07-13 17:24:00 +1000

    Remove IE-specific renderer

--- CHANGES.rst
+++ CHANGES.rst
@@ -6,9 +6,6 @@ CHANGES
 
 - Add ``startingPoint`` configuration item for d3_cloud.
   [davidjb]
-- Render d3_cloud's script tag with conditional comment for IE >=9 (what
-  ``js.d3`` supports).
-  [davidjb]
 
 
 1.0.1 (2012-12-21)
--- js/d3_cloud/__init__.py
+++ js/d3_cloud/__init__.py
@@ -1,10 +1,15 @@
 from fanstatic import Library, Resource
-from js.d3 import d3, render_js_d3_new_ie
+from js.d3 import d3
 
-library = Library('d3_word_cloud', 'resources')
 
-d3_cloud = Resource(library, 'd3.layout.cloud.js',
-                    minified='d3.layout.cloud.min.js',
-                    depends=(d3,),
-                    renderer=render_js_d3_new_ie
-                   )
+library = Library(
+    'd3_word_cloud',
+    'resources'
+)
+
+d3_cloud = Resource(
+    library,
+    'd3.layout.cloud.js',
+    minified='d3.layout.cloud.min.js',
+    depends=(d3,),
+)
